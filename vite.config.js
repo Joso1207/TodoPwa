bf6bb3a52ef153d6fd1bd864a70c5d6073f312fd
Joso1207 <70628108+Joso1207@users.noreply.github.com>
2026-02-17 07:45:54 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-    base:"/todopwa"
+    base:"/TodoPwa/"
     server: {
         host: true
     },
